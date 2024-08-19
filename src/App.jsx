36dbd50575268fcac992d83444d9bcd5b81774e6
joamilibarra@gmail.com <joamilibarra@gmail.com>
2024-08-18 23:28:56 -0400

    Portafolio V 1.13

--- src/App.jsx
+++ src/App.jsx
@@ -26,20 +26,6 @@ function App() {
           <Col></Col>
         </Row>
       </Container>
-
-      {/* <div className="header">
-        <div className="container">
-          <img src={fondo} alt="Fondo" className="background" />
-          <img src={avatar} alt="Avatar" className="avatar" />
-          <h1 className="titulo">Full Stack Developer</h1>
-        </div>
-      </div> */}
-
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
     </>
   );
 }
