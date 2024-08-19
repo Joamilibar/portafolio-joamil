e22d39afd6d7b2f6a93200b6db4c56684b08ec16
joamilibarra@gmail.com <joamilibarra@gmail.com>
2024-08-18 20:49:46 -0400

    Portafolio V 1.11

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import { useState } from "react";
 import avatar from "./assets/img/avatar.svg";
 import "./logo.css";
 import fondo from "./assets/img/fondo.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      {/* <Container fluid>
+        <Row>
+          <Col>
+            {" "}
+            <img src={fondo} alt="Fondo" className="background" />
+            <img src={avatar} alt="Avatar" className="avatar" />
+            <h1 className="titulo">Full Stack Developer</h1>
+          </Col>
+        </Row>
+      </Container> */}
+
       <div className="header">
         <div className="container">
           <img src={fondo} alt="Fondo" className="background" />
