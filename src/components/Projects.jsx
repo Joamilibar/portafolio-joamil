0cb58e71facbd49ec4340f6cdd1207f1f1b302af
joamilibarra@gmail.com <joamilibarra@gmail.com>
2024-08-20 03:23:28 -0400

    Portafolio V 1.137

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,34 +14,31 @@ function Projects() {
           <Col>
             <div className="project">
               {projects.map((project) => (
-                <a key={project.image} target="_blank">
+                <a key={project.image}>
                   <div>
                     <article className="article-items">
                       <div>
-                        <div>
-                          <img
-                            href={project.www}
-                            src={project.image}
-                            className="link"
-                            alt="photo"
-                          />
-                        </div>
-                        <div>
-                          <h2>{project.title}</h2>
-                          <h3>{project.subtitle}</h3>
-                          <p>{project.description}</p>
-                          <h3>Tecnologies </h3>
-                          <p>{project.tecnologies}</p>
-                          <h3> - www - </h3>
-                          <p href={project.www} className="link">
-                            {project.www}
-                          </p>
-                          <h3>Github </h3>
-                          <span className="button">
-                            <p>{project.repositorie}</p>
-                            <img src={github} className="" alt="Logo GitHub" />
-                          </span>
-                        </div>
+                        <img
+                          href={project.www}
+                          src={project.image}
+                          className="link"
+                          alt="photo"
+                        />
+                      </div>
+                      <div>
+                        <h2>{project.title}</h2>
+                        <h3>{project.subtitle}</h3>
+                        <p>{project.description}</p>
+                        <h3>Tecnologies </h3>
+                        <p>{project.tecnologies}</p>
+                        <h3> - www - </h3>
+                        <p href={project.www} className="link">
+                          {project.www}
+                        </p>
+                        <h3>Github </h3>
+                        <span className="button">
+                          <img src={github} className="" alt="Logo GitHub" />
+                        </span>
                       </div>
                     </article>
                   </div>
