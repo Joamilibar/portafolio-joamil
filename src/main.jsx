eb36907cfb2a1b959178950494b793950dd3c561
joamilibarra@gmail.com <joamilibarra@gmail.com>
2026-02-11 01:33:26 -0300

    Ajuste secciones

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Projects from "./components/Projects.jsx";
-import Tecnologies from "./components/Tecnologies.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Tecnologies />
-    <Projects />
   </StrictMode>
 );
